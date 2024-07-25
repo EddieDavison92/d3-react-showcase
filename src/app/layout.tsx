1e22a59b17ddb5a191605eeed84b4ec08eb39999
EddieDavison92 <143042680+EddieDavison92@users.noreply.github.com>
2024-07-25 16:16:07 +0100

    no flash script take 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,12 @@ interface RootLayoutProps {
 const noFlashScript = `
   (function() {
     try {
-      var mode = localStorage.getItem('theme') || 'light';
-      if (mode === 'system') {
+      var theme = localStorage.getItem('theme') || 'light';
+      if (theme === 'system') {
         var systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        mode = systemPrefersDark ? 'dark' : 'light';
+        theme = systemPrefersDark ? 'dark' : 'light';
       }
-      document.documentElement.classList.add(mode);
+      document.documentElement.className = theme;
     } catch (e) {}
   })();
 `;
