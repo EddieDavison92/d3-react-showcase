8eae2f86716e2e174665407e025958026402e6a3
EddieDavison92 <143042680+EddieDavison92@users.noreply.github.com>
2024-08-07 22:05:19 +0100

    wip greek gods network graph

--- src/app/network-graph/page.tsx
+++ src/app/network-graph/page.tsx
@@ -5,7 +5,8 @@ import GreekGodsGraph from '@/components/d3/GreekGodsGraph'
 
 const Page: React.FC = () => {
   return (
-    <div>
+    <div className="mx-auto">
+      <h1 className="text-2xl text-center mt-12 font-semibold">WIP: Force Directed Network Graph</h1>
       <GreekGodsGraph />
     </div>
   );
